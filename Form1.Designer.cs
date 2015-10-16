bbd2ca3cad5cad1b11fd47427bb12fe2b33e8936
Henrik Mårtensson <preox@kth.se>
2015-10-16 09:07:05 +0200

    First commit
    First commit. Just one algorithm so far.

---
+++ Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace SortingAlgrorithms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.scrambleListButton = new System.Windows.Forms.Button();
+            this.quitButton = new System.Windows.Forms.Button();
+            this.quickSortButton = new System.Windows.Forms.Button();
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 4F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 6;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(261, 508);
+            this.listBox1.TabIndex = 0;
+            // 
+            // scrambleListButton
+            // 
+            this.scrambleListButton.Location = new System.Drawing.Point(279, 12);
+            this.scrambleListButton.Name = "scrambleListButton";
+            this.scrambleListButton.Size = new System.Drawing.Size(102, 23);
+            this.scrambleListButton.TabIndex = 1;
+            this.scrambleListButton.Text = "Scramble List";
+            this.scrambleListButton.UseVisualStyleBackColor = true;
+            this.scrambleListButton.Click += new System.EventHandler(this.scrambleListButton_Click);
+            // 
+            // quitButton
+            // 
+            this.quitButton.Location = new System.Drawing.Point(298, 497);
+            this.quitButton.Name = "quitButton";
+            this.quitButton.Size = new System.Drawing.Size(102, 23);
+            this.quitButton.TabIndex = 2;
+            this.quitButton.Text = "Quit Program";
+            this.quitButton.UseVisualStyleBackColor = true;
+            this.quitButton.Click += new System.EventHandler(this.quitButton_Click);
+            // 
+            // quickSortButton
+            // 
+            this.quickSortButton.Location = new System.Drawing.Point(279, 52);
+            this.quickSortButton.Name = "quickSortButton";
+            this.quickSortButton.Size = new System.Drawing.Size(75, 23);
+            this.quickSortButton.TabIndex = 3;
+            this.quickSortButton.Text = "QuickSort";
+            this.quickSortButton.UseVisualStyleBackColor = true;
+            this.quickSortButton.Click += new System.EventHandler(this.quickSortButton_Click);
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.Location = new System.Drawing.Point(360, 57);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(74, 13);
+            this.timeLabel.TabIndex = 4;
+            this.timeLabel.Text = "Time Elapsed:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(487, 538);
+            this.Controls.Add(this.timeLabel);
+            this.Controls.Add(this.quickSortButton);
+            this.Controls.Add(this.quitButton);
+            this.Controls.Add(this.scrambleListButton);
+            this.Controls.Add(this.listBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Sorting algorithms visualized";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button scrambleListButton;
+        private System.Windows.Forms.Button quitButton;
+        private System.Windows.Forms.Button quickSortButton;
+        private System.Windows.Forms.Label timeLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace SortingAlgrorithms
+{
+    public partial class Form1 : Form
+    {
+        private static int noOfLines = 80;
+        private Random rnd = new Random();
+        private List<string> dataSource;// = new List<string>();
+        private BindingList<string> blData;// = new BindingList<string>(dataSource);
+
+
+        public Form1()
+        {
+            // Initializing stuff. Listbox and dataconnections as well. 
+            InitializeComponent();
+            dataSource = new List<string>();
+            blData = new BindingList<string>(dataSource);
+            listBox1.DataSource = blData;
+
+            for (int i = 1; i < noOfLines+1; i++ )
+            {
+                blData.Add(getLine(i));
+            }
+            listScrambler();
+
+
+        }
+
+
+        #region Functions
+
+        /*
+         * ListScrambler
+         * Clears list into a temporary list "randomly". 
+         * Then re-adds to existing list. 
+         */
+        public void listScrambler()
+        {
+            List<string> tempList = new List<string>();
+            int randomIndex = 0;
+            while (blData.Count > 0)
+            {
+                randomIndex = rnd.Next(0, blData.Count);
+                tempList.Add(blData[randomIndex]);
+                blData.RemoveAt(randomIndex);
+            }
+            foreach (string st in tempList)
+                blData.Add(st);
+        }
+
+
+        /*
+         * Simply swaps two items. as long as given indexes does not exceed list length. 
+         */
+        private void swap(int indexA, int indexB, BindingList<string> data)
+        {
+            if (indexA < blData.Count && indexB < blData.Count)
+            {
+                string temp = blData[indexA];
+                blData[indexA] = blData[indexB];
+                blData[indexB] = temp;
+            }
+        }
+
+        /*
+         * Returns a string of '#' with given length. 
+         */
+        private string getLine(int length)
+        {
+            string retVal = "";
+            for (int i = 0; i < length; i++ )
+            {
+                retVal += "#";
+            }
+            return retVal;
+        }
+
+
+        /*
+         * Quicksort. Or at least, an attempt at one. 
+         * Based upon https://en.wikipedia.org/wiki/Quicksort
+         * Note that pivot isn't randomly choosen. Not optimal
+         */
+        private void quickSort(BindingList<string> data,  int low, int high)
+        {
+            if (low < high)
+            {
+                int p = partition(data, low, high);
+                quickSort(data, low, p - 1);
+                quickSort(data, p + 1, high);
+            }
+        }
+        // Needed for quicksort. 
+        private int partition(BindingList<string> data, int low, int high)
+        {
+            int pivot = data[high].Length;
+            int i = low;
+            for (int j = low; j < high; j++)
+            {
+                if (data[j].Length <= pivot)
+                {
+                    swap(i, j, data);
+                    i++;
+                }
+            }
+            swap(i, high, data);
+            return i;
+        }
+
+        #endregion
+
+
+        /*
+         * All eventhandlers for button and suchs
+         */
+        #region Eventhandlers
+
+        /*
+         * handler for scambleListButton
+         * Scrambles list. Sort of....
+         */
+        private void scrambleListButton_Click(object sender, EventArgs e)
+        {
+            listScrambler();
+            timeLabel.Text = "Time Elapsed: ";
+        }
+
+        // Duuuh. 
+        private void quitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+
+        private void quickSortButton_Click(object sender, EventArgs e)
+        {
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+
+            quickSort(blData, 0, blData.Count - 1);
+            timer.Stop();
+            timeLabel.Text = "Time elapsed: " + timer.ElapsedMilliseconds + " ms";
+        }
+
+
+        #endregion
+
+
+    }
+}
